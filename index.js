55c9064c2b7bffd5e56e82ba43d4a29340fb78b5
Git-Store <hetader489@futurejs.com>
2025-10-13 15:42:48 +0000

    HEllo world API

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const { add } = require("./math");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const sum = add(2,3);
-console.log(sum);
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello, world!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
