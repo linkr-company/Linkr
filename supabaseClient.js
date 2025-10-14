f0c4ff075fac37d06099ac446393fbcd2f332578
Git-Store <hetader489@futurejs.com>
2025-10-14 14:19:55 +0000

    DB url not found error(env variable updated)

--- supabaseClient.js
+++ supabaseClient.js
@@ -8,4 +8,7 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
+console.log("SUPABASE_URL:", process.env.SUPABASE_URL ? "SET" : "NOT SET");
+console.log("SUPABASE_SERVICE_ROLE_KEY:", process.env.SUPABASE_SERVICE_ROLE_KEY ? "SET" : "NOT SET");
+
 export default supabase;
